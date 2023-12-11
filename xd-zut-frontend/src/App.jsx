423f3140c6091f2b1d8eb63dabc724f1cf9e5a67
Jakub Znyk <zj49454@zut.edu.pl>
2023-12-11 16:32:49 +0100

    feat: add functionless rate page

--- xd-zut-frontend/src/App.jsx
+++ xd-zut-frontend/src/App.jsx
@@ -1,7 +1,65 @@
-import { Button } from '@mantine/core'
+import {
+  ActionIcon,
+  Button,
+  Flex,
+  Stack,
+  TextInput,
+  Textarea,
+  Title,
+  UnstyledButton,
+} from '@mantine/core'
+import { IconMoodHappy, IconMoodSad } from '@tabler/icons-react'
+import { useState } from 'react'
 
 function App() {
-  return <Button>xD</Button>
+  const [rate, setRate] = useState()
+
+  return (
+    <Stack w='100%' h='100%' align='center' justify='center' spacing='xl'>
+      <Title>Jak podobały ci się zajęcia?</Title>
+      <Flex w='100%' justify='center' gap='xl'>
+        <Button h='10rem' w='10rem' variant='light' color='red'>
+          <IconMoodSad
+            size='10rem'
+            onClick={() => {
+              setRate(-1)
+            }}
+          />
+        </Button>
+        <Button h='10rem' w='10rem' variant='light' color='green'>
+          <IconMoodHappy
+            size='10rem'
+            onClick={() => {
+              setRate(1)
+            }}
+          />
+        </Button>
+      </Flex>
+      {rate && (
+        <>
+          <Textarea
+            label='Dodaj komentarz do oceny'
+            placeholder='Komentarz'
+            variant='filled'
+            radius='lg'
+            size='lg'
+            minRows={6}
+            maxRows={6}
+            p={'xl'}
+            w='80%'
+          />
+          <Flex gap='xl'>
+            <Button size='xl' variant='outline' miw='15rem'>
+              Zmień ocenę
+            </Button>
+            <Button size='xl' miw='15rem'>
+              Wyślij
+            </Button>
+          </Flex>
+        </>
+      )}
+    </Stack>
+  )
 }
 
 export default App
--- xd-zut-frontend/src/main.jsx
+++ xd-zut-frontend/src/main.jsx
@@ -2,10 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { MantineProvider } from '@mantine/core'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        globalStyles: theme => ({
+          '*, *::before, *::after': {
+            boxSizing: 'border-box',
+          },
+
+          body: {
+            ...theme.fn.fontStyles(),
+            backgroundColor:
+              theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+            color:
+              theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+            lineHeight: theme.lineHeight,
+          },
+
+          '.your-class': {
+            backgroundColor: 'red',
+          },
+
+          '#your-id > [data-active]': {
+            backgroundColor: 'pink',
+          },
+        }),
+      }}
+    >
       <App />
     </MantineProvider>
   </React.StrictMode>
