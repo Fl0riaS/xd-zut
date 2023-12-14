c9c96b452055c5de2e43081acd12d58ee2ff34d8
Filip Urban <furban@codeton.pl>
2023-12-14 09:21:23 +0100

    feat: excel

--- xd-zut-backend/xdzut/src/Controller/OpinionController.php
+++ xd-zut-backend/xdzut/src/Controller/OpinionController.php
@@ -8,6 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Annotation\MapRequestPayload;
 use App\Model\AddOpinionDTO;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
 
 class OpinionController extends AbstractController
 {
@@ -31,20 +36,49 @@ class OpinionController extends AbstractController
     // comment
     #[Route('/opinion/add', name: 'app_opinion_add', methods: ['POST'])]
     public function addOpinion(
-      // Request $request,
+      Request $request,
       // SerializerInterface $serializer,
       #[MapRequestPayload] AddOpinionDTO $opinionDTO
       ): JsonResponse
     {
 
-        die(json_encode($opinionDTO));
         // get data from request
-        // $data = json_decode($request->getContent(), true);
+        $data = json_decode($request->getContent(), true);
 
-        // return data as json
-        // return $this->json([
-          // "data" => $data
-        // ]);
+        return $this->json([
+          "data" => $data
+        ]);
         
     }
+
+  #[Route('/excel/generate', name: 'app_excel_create', methods: ['GET'])]
+  public function generateExcel(): Response
+  {
+      $spreadsheet = new Spreadsheet();
+      $sheet = $spreadsheet->getActiveSheet();
+
+      $data = [
+          ['Header1', 'Header2', 'Header3'],
+          [1, 2, 3],
+          [4, 5, 6],
+          // ...
+      ];
+
+      // Adding data to spreadsheet
+      $sheet->fromArray($data, null, 'A1');
+
+      // Create writer and save to file
+      $writer = new Xlsx($spreadsheet);
+
+      // Create http response
+      $response = new StreamedResponse(function() use ($writer) {
+          $writer->save('php://output');
+      });
+
+      // Setting up correct headers
+      $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      $response->headers->set('Content-Disposition', 'attachment;filename="export.xlsx"');
+      $response->headers->set('Cache-Control','max-age=0');
+      return $response;
+  }
 }
