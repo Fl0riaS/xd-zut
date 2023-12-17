b3e6fe3bc23ccf9bd084ad59d25ef36b460d2ca7
Filip Urban <furban@codeton.pl>
2023-12-17 23:41:49 +0100

    report resend

--- xd-zut-backend/xdzut/src/Controller/OpinionController.php
+++ xd-zut-backend/xdzut/src/Controller/OpinionController.php
@@ -210,4 +210,81 @@ class OpinionController extends AbstractController
 
         return $response;
     }
+
+    // Endpoint for resend email with raport
+    #[Route('/raport/{id}/resend', name: 'app_raport_resend', methods: ['GET'])]
+    public function resendRaport(Raport $raport, MailerInterface $mailer): JsonResponse
+    {
+
+        // generate raport
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Ustawianie danych raportu
+        $sheet->setCellValue('A1', 'Raport');
+        $sheet->setCellValue('A2', 'Data');
+        $sheet->setCellValue('B2', $raport->getDate()->format('Y-m-d'));
+        $sheet->setCellValue('A3', 'Prowadzący');
+        $sheet->setCellValue('B3', $raport->getCourse()->getTeacher()->getName());
+        $sheet->setCellValue('A4', 'Przedmiot');
+        $sheet->setCellValue('B4', $raport->getCourse()->getSubject()->getTitle());
+        $sheet->setCellValue('A5', 'Grupa');
+        $sheet->setCellValue('B5', $raport->getCourse()->getGroupName());
+        $sheet->setCellValue('A6', 'Ocena miesiąca');
+        $sheet->setCellValue('B6', $raport->getTotalScore());
+
+        // Ocena z ostatnich zajec to suma wszystkich ocen z tego raportu
+        $last_lecture_score = 0;
+
+        foreach ($raport->getOpinions() as $opinion) {
+            $last_lecture_score += $opinion->getScore();
+        }
+
+        $sheet->setCellValue('A7', 'Ocena całkowita');
+        $sheet->setCellValue('B7', $raport->getMonthScore());
+        $sheet->setCellValue('A8', 'Ocena z ostatnich zajęć');
+        $sheet->setCellValue('B8', $last_lecture_score);
+
+        // Ustawianie opinii
+        $sheet->setCellValue('A10', 'Opinie');
+        // $sheet->setCellValue('A9', 'Data');
+        $sheet->setCellValue('B11', 'Ocena');
+        $sheet->setCellValue('C11', 'Komentarz');
+
+        $row = 12;
+        foreach ($raport->getOpinions() as $opinion) {
+            // $sheet->setCellValue('A' . $row, $opinion->getDate()->format('Y-m-d'));
+            $sheet->setCellValue('B' . $row, $opinion->getScore());
+            $sheet->setCellValue('C' . $row, $opinion->getComment());
+            $row++;
+        }
+
+        // add raport to email
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('raport.xlsx');
+
+        // send email
+
+        $teacher_email = $raport->getCourse()->getTeacher()->getEmail();
+
+        $email = (new Email())
+            ->from('xdzut@interia.pl')
+            ->to($teacher_email)
+            ->subject('Raport')
+            ->text('Raport')
+            ->attachFromPath('raport.xlsx');
+
+
+        $mailer->send($email);
+
+        // delete raport
+        unlink('raport.xlsx');
+
+        $response = new JsonResponse();
+        $response->setStatusCode(Response::HTTP_OK);
+
+        return $response;
+
+    }
 }
